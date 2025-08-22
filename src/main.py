dfcc6fe3adc6ce52533c95a4df57fe1767f08795
Agor Jeffery Kofi <91951535+UltronXx@users.noreply.github.com>
2025-08-22 10:28:44 -0100

    Organised lines

--- src/main.py
+++ src/main.py
@@ -1,50 +1,76 @@
 import flet as ft
-import utils.date_picker_example as date_picker_example
 from flet import (
-Container, FontWeight,
-TextField,CupertinoButton,
-Icons,app,ElevatedButton,
-Column,Row,Stack,
-Text,CupertinoTextField,
-Padding,Colors,OutlinedButton,
-Alignment,alignment
+    Container,
+    FontWeight,
+    TextField,
+    CupertinoButton,
+    Icons,
+    app,
+    Page,
+    ElevatedButton,
+    Column,
+    Row,
+    Stack,
+    Text,
+    CupertinoTextField,
+    Padding,
+    Colors,
+    OutlinedButton,
+    Alignment,
+    alignment,
+    MainAxisAlignment,
+    ButtonStyle,
+    FilledButton
 )
 
+import utils.date_picker_example as date_picker_example
 
-def main(page: ft.Page):
+def main(page: Page):
     page.title = "Nexus Attendance"
     page.padding = 20
-    #page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
     upper_ = Container(
         content= Row(
             controls=[
-                Text("Time and Attendance",
-                     size=27,font_family='Poppins',
-                     weight=FontWeight.W_500),
-                ft.FilledButton(text = "Record Attendance",
-                                height=40,
-                                width=190,
-                                color = 'white',
-                                icon=Icons.ADD,
-                                style=ft.ButtonStyle(bgcolor=Colors.BLUE_900))
-                ],
-                alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
-            ),
-            bgcolor=Colors.GREY_300,
-            border_radius=17,
-            width=800,
-            height=80,
-            padding=15
+                Text(
+                    value="Time and Attendance",
+                    size=27,
+                    font_family='Poppins',
+                    weight=FontWeight.W_500
+                ),
+                FilledButton(
+                    text = "Record Attendance",
+                    height=40,
+                    width=190,
+                    color = 'white',
+                    icon=Icons.ADD,
+                    style=ButtonStyle(
+                        bgcolor=Colors.BLUE_900
+                    )
+                )
+            ],
+            alignment=MainAxisAlignment.SPACE_BETWEEN,
+        ),
+        bgcolor=Colors.GREY_300,
+        border_radius=17,
+        width=800,
+        height=80,
+        padding=15
     )
 
     datepart = Container(
         content= Row(
             controls=[
-                OutlinedButton("Date", icon=Icons.CALENDAR_MONTH_ROUNDED),
-                OutlinedButton("Search Employee", icon=Icons.SEARCH_OUTLINED)
+                OutlinedButton(
+                    text="Date",
+                    icon=Icons.CALENDAR_MONTH_ROUNDED
+                ),
+                OutlinedButton(
+                    text="Search Employee",
+                    icon=Icons.SEARCH_OUTLINED
+                )
             ],
-            alignment=ft.MainAxisAlignment.START
+            alignment=MainAxisAlignment.START
         )
     )
 
@@ -58,4 +84,4 @@ def main(page: ft.Page):
     )
 
 if __name__=='__main__':
-    app(target=main,assets_dir='assets')
+    app(target=main, assets_dir='assets')
